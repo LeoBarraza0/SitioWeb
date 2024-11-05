39afd8e3bc772b6ad71791b8f93671a36618a57d
Leonardo Barraza <leonardoa-barrazac@unilibre.edu.co>
2024-11-05 16:01:50 -0500

    Se ha agregado el formulario

---
+++ src/scripts/scripts.js
@@ -0,0 +1,12 @@
+const form = document.getElementById('contactForm');
+form.addEventListener('submit', function(event){
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    if(name === '' || email === ''){
+        alert('Por favor, completa todos los campos.');
+        event.preventDefault();
+    }else{
+        alert('¡Formulario enviado correctamente!');
+        event.preventDefault();
+    }
+});
