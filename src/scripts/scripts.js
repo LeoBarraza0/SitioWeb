999006eb5404b5da4b7684a04a2372b4c87d28e7
Jesús Barros <jesus.reinaldo.barros@gmail.com>
2024-11-06 08:59:37 -0500

    Se modificó el formulario

--- src/scripts/scripts.js
+++ src/scripts/scripts.js
@@ -1,12 +1,15 @@
-const form = document.getElementById('contactForm');
-form.addEventListener('submit', function(event){
-    const name = document.getElementById('name').value;
-    const email = document.getElementById('email').value;
-    if(name === '' || email === ''){
-        alert('Por favor, completa todos los campos.');
-        event.preventDefault();
-    }else{
-        alert('¡Formulario enviado correctamente!');
-        event.preventDefault();
-    }
+document.addEventListener('DOMContentLoaded', function() {
+    const form = document.getElementById('contactForm');
+    form.addEventListener('submit', function(event) {
+        const name = document.getElementById('name').value.trim();
+        const email = document.getElementById('email').value.trim();
+        
+        if (name === '' || email === '') {
+            alert('Por favor, completa todos los campos.');
+            event.preventDefault(); // Evita el envío del formulario si los campos están vacíos
+        } else {
+            alert('¡Formulario enviado correctamente!');
+            // Aquí puedes permitir que el formulario se envíe quitando event.preventDefault()
+        }
+    });
 });
